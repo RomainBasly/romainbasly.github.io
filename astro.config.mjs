1d67adb6ad30315bacfca8b318b997c4c26197b8
Romain Basly <romain.basly@protonmail.com>
2024-12-07 16:15:15 +0100

    fix the sitemap path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import astroIcon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astronaut.github.io",
-  integrations: [react(), mdx(), astroIcon()],
+  site: "https://romainbasly.github.io",
+  integrations: [react(), mdx(), astroIcon(), sitemap()],
   output: "static",
 });
