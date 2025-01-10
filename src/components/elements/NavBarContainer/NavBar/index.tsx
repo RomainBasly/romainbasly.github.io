dac6aaee0f336e6269b188394dd6b0a53b38ec8c
Romain Basly <romain.basly@protonmail.com>
2025-01-10 19:17:39 +0100

    modify the resume + update goals

--- src/components/elements/NavBarContainer/NavBar/index.tsx
+++ src/components/elements/NavBarContainer/NavBar/index.tsx
@@ -37,7 +37,7 @@ const Navbar = ({
           </div>
         </div>
         <div className="resume-container">
-          <a href="/CV/CV_Romain_Basly_nov2024.pdf" download>
+          <a href="/CV/CV_Romain_Basly_jan2025.pdf" download>
             <button className="download-resume">Resume</button>
           </a>
         </div>
